58b48c8322fc6b8e6436791c4deaf2e406faa793
leospace <leo_space@163.com>
2014-12-02 14:42:32 +0800

    Update SQL schema

--- ZhongDing.Database/dbo/Tables/DepartmentProductSalesBonus.sql
+++ ZhongDing.Database/dbo/Tables/DepartmentProductSalesBonus.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[DepartmentProductSalesBonus] (
     [ID]                INT        IDENTITY (1, 1) NOT NULL,
     [SalesPlanID]       INT        NOT NULL,
+    [SalesPlanTypeID]   INT        NOT NULL,
     [CompareOperatorID] INT        NOT NULL,
     [SalesPrice]        MONEY      NULL,
     [BonusRatio]        FLOAT (53) NULL,
@@ -12,4 +13,6 @@
     CONSTRAINT [PK_DepartmentProductSalesBonus] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_DepartmentProductSalesBonus_DepartmentProductSalesPlan] FOREIGN KEY ([SalesPlanID]) REFERENCES [dbo].[DepartmentProductSalesPlan] ([ID])
 );
+
+
 
