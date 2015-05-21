d2a4975885f6762fecfb7a9456a203cdb7ab687c
wangning08115 <715617887@qq.com>
2015-05-21 13:50:16 +0800

    add files

---
+++ ZhongDing.Common/Enums/ECautionMoneyTypeCategory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZhongDing.Common.Enums
+{
+    /// <summary>
+    /// 保证金类别
+    /// </summary>
+    public enum ECautionMoneyTypeCategory : int
+    {
+        /// <summary>
+        /// 供应商 1
+        /// </summary>
+        Supplier = 1,
+        /// <summary>
+        /// 客户 2
+        /// </summary>
+        Client = 2
+    }
+}
