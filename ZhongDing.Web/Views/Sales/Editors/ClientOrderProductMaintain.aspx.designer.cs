8657dc92ffbf9d6a480ea2771de0a2ec21f9fc43
leospace <leo_space@163.com>
2015-06-08 16:48:02 +0800

    fixed bug for add sales order
    fixed bug for add sales order to resolve the product specification when
    change the selected warehouse

--- ZhongDing.Web/Views/Sales/Editors/ClientOrderProductMaintain.aspx.designer.cs
+++ ZhongDing.Web/Views/Sales/Editors/ClientOrderProductMaintain.aspx.designer.cs
@@ -67,6 +67,15 @@ namespace ZhongDing.Web.Views.Sales.Editors {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator rfvWarehouse;
         
         /// <summary>
+        /// divProducts control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl divProducts;
+        
+        /// <summary>
         /// rcbxProduct control.
         /// </summary>
         /// <remarks>
